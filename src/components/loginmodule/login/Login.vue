4a8042a9dda9fd377c08749786abb34d48cd829c
baifuchao52013 <296718458@qq.com>
2018-07-19 17:58:37 +0800

    第二次上传

---
+++ src/components/loginmodule/login/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="module-login" v-if="showLogin">
+    <login-box v-bind:title="title" v-on:sendUserToParent="getUserFromChild"></login-box>
+  </div>
+</template>
+
+<script>
+  import LoginBox from './LoginBox'
+    export default {
+        name: "login",
+      components: {LoginBox},
+      data(){
+          return{
+            title:"中国湿地",
+            showLogin:true,
+            user:""
+          }
+      },
+      methods:{
+          getUserFromChild:function(data){
+            this.user=data;
+            console.log(this.user);
+          }
+      }
+    }
+</script>
+
+<style scoped>
+    .module-login{
+      position: absolute;
+      top: 30%;
+      right: 15%;
+    }
+</style>
---
+++ src/components/loginmodule/login/LoginBox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login-box">
+    <p>{{title}}</p>
+    <div>
+      <login-form v-on:getChildUser="getChildUser"></login-form>
+    </div>
+  </div>
+</template>
+<script>
+  import LoginForm from './LoginForm'
+  export default {
+    name: "LoginBox",
+    components: {LoginForm},
+    data(){
+      return{
+        user:""
+      }
+    },
+    props: {
+      title: {
+        type: String
+      }
+    },
+    methods:{
+      getChildUser:function(data){
+        this.user=data;
+        this.$emit("sendUserToParent",this.user);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .login-box {
+    width: 350px;
+    height: 270px;
+    border: 1px solid white;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 0px auto;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .login-box > p {
+    color: white;
+    font-family: "黑体";
+    margin: 15px auto;
+    text-align: center;
+    font-size: 18px;
+  }
+</style>
