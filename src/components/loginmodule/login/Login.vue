00c59811219ba427790b54443c1840c8431d1f3c
baifuchao52013 <296718458@qq.com>
2018-07-20 10:37:09 +0800

    修改登录

--- src/components/loginmodule/login/Login.vue
+++ src/components/loginmodule/login/Login.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="module-login" v-if="showLogin">
-    <login-box v-bind:title="title" v-on:sendUserToParent="getUserFromChild"></login-box>
+    <login-box v-bind:title="title" v-on:sendUserToParent="loginSystem"></login-box>
   </div>
 </template>
 
 <script>
   import LoginBox from './LoginBox'
+
     export default {
         name: "login",
       components: {LoginBox},
@@ -16,10 +17,36 @@
             user:""
           }
       },
+
       methods:{
-          getUserFromChild:function(data){
+        loginSystem:function(data){
             this.user=data;
             console.log(this.user);
+            if (data) {
+              this.$axios.post("/apis/user/login",{username:this.user.username,password:this.user.password})
+                .then(data =>{
+                  console.log(data)
+                })
+            /*  if (this.user.username === 'admin' && this.user.password === '123') {
+                // dispatch采用Promise链式调用
+                this.$store.dispatch('login', this.user.username).then(() => {
+                  this.$notify({
+                    type: 'success',
+                    message: '欢迎你,' + this.user.username + '!',
+                    duration: 3000
+                  })
+                })
+              } else {
+                this.$message({
+                  type: 'error',
+                  message: '用户名或密码错误',
+                  showClose: true
+                })
+              }*/
+            }
+            else {
+              return false
+            }
           }
       }
     }
--- src/main.js
+++ src/main.js
@@ -6,14 +6,19 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
 import $ from 'jquery'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-
+Vue.prototype.$axios = axios;
+/*axios.defaults.headers.common['token'] = "";*/
+axios.defaults.headers.post["Content-type"] = "application/json"
+import store from './store/store'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
